21554314b04d0c67108476c3ec48229cac87c583
Eldorbek <raxmatoveldor37@gmail.com>
2023-03-16 09:19:23 +0500

    each buttons and inputs files changed path to other directory

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import './styles/App.css'
 import PostList from "./companents/PostList";
 import MyButton from "./companents/UI/button/MyButton";
+import MyInput from "./companents/UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,12 +12,33 @@ function App() {
     { id: 3, title: 'C++', body: 'Description 3' }
   ])
 
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const addNewPost = (event) => {
+    event.preventDefault();
+    const newPost = {
+      title,
+      body
+    }
+    console.log(newPost);
+  }
+
   return (
     <div className="App">
-      <form>
-        <input type="text" placeholder="title of post" />
-        <input type="text" placeholder="description of post" />
-        <MyButton>Create post</MyButton>
+      <form onSubmit={addNewPost}>
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          required
+          placeholder="title of post" />
+        <MyInput
+          value={body}
+          onChange={e => setBody(e.target.value)}
+          type="text"
+          placeholder="description of post" />
+        <MyButton type="submit">Create post</MyButton>
       </form>
       <PostList posts={posts} title="List of posts" />
     </div>
